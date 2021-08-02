b500449d6b3b1816febe5baf2b1bf10790bf503b
CedricLange <langemataso@gmail.com>
2021-08-02 15:17:40 +0200

    test ced

--- application/modules/product/migrations/001_initial_product_.php
+++ application/modules/product/migrations/001_initial_product_.php
@@ -13,26 +13,41 @@ class Migration_initial_product extends CI_Migration {
     public function up() {
         $this->dbforge->drop_table('vehicule',TRUE);
         $this->dbforege->add_field([
-            'id' =>[
+            've_id' =>[
                 'type' => 'MEDIUMINT',
-                'constraint' => '4',
+                'constraint' => '5',
                 'auto_increment' => TRUE
             ],
-            'name' =>[
+            've_name' =>[
                 'type' => 'VARCHAR',
                 'constraint' => '50',
             ],
-            'marque' => [
+            've_brand' => [
                 'type' => 'VARCHAR',
                 'constraint' => '50'
             ],
-            'model' =>[
+            've_model' =>[
                 'type' => 'VARCHAR',
                 'constraint' => '50'
             ]          
         ]);
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('vehicule');
+
+        $this->dbforge->drop_table('categories',TRUE);
+        $this->dbforge->add_field([
+
+            'cat_id' => [
+                'type' => 'MEDUIMINT',
+                'constraint' => '5',
+                'auto_increment' => TRUE
+            ],
+            'cat_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ]
+        ]);
+
     }
             
     public function down() {
