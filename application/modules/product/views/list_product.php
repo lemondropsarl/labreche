108792d7e28306c284a2d6966d55fd74c649127e
Bob <bobmwenge@gmail.com>
2021-08-04 11:26:56 +0200

    stage file

--- application/modules/product/views/list_product.php
+++ application/modules/product/views/list_product.php
@@ -1,58 +1,37 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 ?>
 <div class="content">
 	<div class="container-fluid">
 		<!-- your content here -->
 		<div class="row">
+<<<<<<< HEAD
+			<div class="col-md-12">
+
+=======
             <div class="col-md-12">
-				<div class="row">
-					<div class="col-lg-3">
-
-						<div class="small-box bg-info">
-							<div class="inner">
-								<h3>1500</h3>
-								<p>categorie</p>
-							</div>
-							<div class="icon">
-								<i class=""></i>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3">
-
-						<div class="small-box bg-purple">
-							<div class="inner">
-								<h3>1500</h3>
-								<p>Accessoire</p>
-							</div>
-							<div class="icon">
-								<i class="fas fa-car"></i>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3">
-
-						<div class="small-box bg-primary">
-							<div class="inner">
-								<h3>1500</h3>
-								<p>moteur</p>
-							</div>
-							<div class="icon">
-								<i class=""></i>
-							</div>
-						</div>
-					</div>
-				</div>
 				<div class="card card-outline card-secondary">
 					<div class="card-header">
 						<h4 class="card-title">Liste des articles</h4>
 						<div class="card-tools">
-								<button type="button" class="btn btn-tool" data-card-widget="collapse"><i
-									class="fas fa-minus"></i></button>
+							<button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
 						</div>
-
+					</div>
+					<div class="row" style="margin-top:1%;">
+						<div class="dropdown col-md-4 col-sm-10 col-xs-10 offset-md-2 form-group">
+							<button type="button" class="btn btn-primary dropdown-toggle form-control col-sm-10 col-xs-10" data-toggle="dropdown">
+								Catégorie
+							</button>
+							<div class="dropdown-menu">
+								<a class="dropdown-item" href="#">catégorie 1</a>
+								<a class="dropdown-item" href="#">catégorie 2</a>
+								<a class="dropdown-item" href="#">catégorie 3</a>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-10 col-xs-10 form-group" >
+							<input type="search" name="" id="" class="form-control col-sm-10 col-xs-10" placeholder="Search">
+						</div>
 					</div>
 					<div class="card-body">
 						<div class="table-responsive">
@@ -66,21 +45,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									</tr>
 								</thead>
 								<tbody>
-									<?php foreach ($products as $item) {?>
-										
+									<?php foreach ($products as $item) { ?>
+
 										<tr>
-											<th><?php echo $item['product_name'];?></th>
-											<th><?php echo $item['product_code'];?></th>
-											<th><?php echo $item['unit_price'];?></th>
+											<th><?php echo $item['product_name']; ?></th>
+											<th><?php echo $item['product_code']; ?></th>
+											<th><?php echo $item['unit_price']; ?></th>
 										</tr>
-									<?php }?>
+									<?php } ?>
 								</tbody>
 							</table>
 						</div>
 					</div>
 				</div>
-				
-				
+
+
 			</div>
 		</div>
 	</div>
