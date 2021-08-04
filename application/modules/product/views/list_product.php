4be8fa45202949b9ed54031069c89bbfd70367b7
CEDRIC MATASO <langemataso@gmail.com>
2021-08-04 11:11:01 +0200

    add fix to card

--- application/modules/product/views/list_product.php
+++ application/modules/product/views/list_product.php
@@ -7,6 +7,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<!-- your content here -->
 		<div class="row">
             <div class="col-md-12">
+				<div class="row">
+					<div class="col-lg-3">
+
+						<div class="small-box bg-info">
+							<div class="inner">
+								<h3>1500</h3>
+								<p>categorie</p>
+							</div>
+							<div class="icon">
+								<i class=""></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3">
+
+						<div class="small-box bg-purple">
+							<div class="inner">
+								<h3>1500</h3>
+								<p>Accessoire</p>
+							</div>
+							<div class="icon">
+								<i class="fas fa-car"></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3">
+
+						<div class="small-box bg-primary">
+							<div class="inner">
+								<h3>1500</h3>
+								<p>moteur</p>
+							</div>
+							<div class="icon">
+								<i class=""></i>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div class="card card-outline card-secondary">
 					<div class="card-header">
 						<h4 class="card-title">Liste des articles</h4>
